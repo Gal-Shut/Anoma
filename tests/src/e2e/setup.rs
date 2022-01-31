cdb764572fbfc7deed96317b0a9e64333fdc13c0
Tomáš Zemanovič <tomas@heliax.dev>
2022-01-31 17:49:14 +0100

    tests/e2e: switch to run tested cmds with info log instead of debug

--- tests/src/e2e/setup.rs
+++ tests/src/e2e/setup.rs
@@ -566,7 +566,7 @@ where
         cmd.release()
     };
     let mut cmd = cmd.run().unwrap().command();
-    cmd.env("ANOMA_LOG", "anoma=debug")
+    cmd.env("ANOMA_LOG", "anoma=info")
         .current_dir(working_dir)
         .args(&["--base-dir", &base_dir.as_ref().to_string_lossy()])
         .args(args);
